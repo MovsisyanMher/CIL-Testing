7dd47cb381130fd8ef6a3475ad7927533546254e
MovsisyanMher <mher.movsisyan2011@yandex.ru>
2018-12-19 10:25:01 -0800

    DevPush 1

---
+++ SimpleCalcAdd/CILTest/CILTestCalcAdd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CILTestCalcAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            int ans = c.Add(10, 84);
+            System.Console.WriteLine("10 + 84 is {0}.", ans);
+            // Wait for user to press the Enter key before shutting down.
+            System.Console.ReadLine();
+        }
+
+        class Calc
+        {
+            public int Add(int x, int y){return x + y;}
+        }
+    }
+}
